9553a444e8fab78856ba476c31191d039d123852
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-21 10:34:55 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // Skapa en klass Dice som representerar en tärning
+        //  - throw = skall ge tärningen ett slumptal 1 - 6
+        //  - getter + setter (validera input)
+        // - 2 konstruktorer (slump + sätt värde)
+
+
+    }
+}
